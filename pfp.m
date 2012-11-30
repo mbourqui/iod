91238188b3bec0b4d0a8ef8efb41b3c1a9ece461
Marc Bourqui <marc.bourqui@epfl.ch>
2012-11-30 11:26:40 +0100

    reputs comments

--- pfp.m
+++ pfp.m
@@ -1,4 +1,13 @@
 function x = pfp(f, x0, epsilon)
+%PFP Trouve une approximation de la solution du probleme suivant en 
+% appliquant la methode de la plus forte pente:
+% min_{x in R^2} (x_1-2)^4 + (x_1-2)^2x_2^2 + (x_2+1)^2
+%   Detailed explanation goes here
+
+% (a) Implementer la methode de la plus forte pente (alog 11.3).
+% Determiner la taille du pas en appliquant la recherche linéaire, algo
+% 11.2 (les deux conditions de Wolfe).
+
     x = x0;
     alpha = 1;
 
@@ -10,16 +19,22 @@ function x = pfp(f, x0, epsilon)
     [~, gfx] = feval(f, x);
     while abs(gfx) > epsilon
         fprintf('Iteration number %d : x = [%f, %f]\n', iteration, x(1), x(2));
+        
+    [~, gfx] = feval(f, x);
+    d = -gfx;
+    alpha = rl(f, x, d, alpha, beta1, beta2, lambda);
+    x = x + alpha * d;
+    iteration = iteration + 1;
+
+% (b) Implementer une fonction qui donne la taille du pas suivant:
+% cf énoncé
+% Quelle est la nature de ce pas? D'ou cette formule vient-elle?
+
 % (c) Comparer le comportement de l'algorithme en utilisant les pas (a) et
 % (b)
 
 % (d) Comparer la methode de plus forte pente et la methode quasi-Newton
 % (qui est deja implementee - Serie 3).
-        
-        [~, gfx] = feval(f, x);
-        d = -gfx;
-        alpha = rl(f, x, d, alpha, beta1, beta2, lambda);
-        x = x + alpha * d;
-        iteration = iteration + 1;
+       
     end
 end
