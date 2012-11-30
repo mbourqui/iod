f8e567ff398cda584298b985aa8501b248006deb
Marc Bourqui <marc.bourqui@epfl.ch>
2012-11-30 10:47:56 +0100

    adds homework statement to pfp.m; adds report specifications in README.md

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 This is the repo for the practical lab in the introduction to differentiable optimization course.
+
+
+Le rapport a rendre pour le 11 janvier 2013 doit contenir:
+ - Code Matlab
+ - Expliquer les questions spécifiques du problème
+ - Expliquer les comparaisons du problème
+ - Les résultats
+
+Longueur < 10 pages.
--- pfp.m
+++ pfp.m
@@ -1,15 +1,22 @@
 function [ output_args ] = pfp( input_args )
-%PFP Trouve une approximation de la solution du problème suivant en 
-% appliquant la méthode de la plus forte pente:
+%PFP Trouve une approximation de la solution du probleme suivant en 
+% appliquant la methode de la plus forte pente:
 % min_{x in R^2} (x_1-2)^4 + (x_1-2)^2x_2^2 + (x_2+1)^2
 %   Detailed explanation goes here
 
-% (a) Implémenter la méthode de la plus forte pente (alog 11.3).
-% Déterminer la taille du pas en appliquant la recherche linéaire, algo
+% (a) Implementer la methode de la plus forte pente (alog 11.3).
+% Determiner la taille du pas en appliquant la recherche linéaire, algo
 % 11.2 (les deux conditions de Wolfe).
 
-% (b) Implémenter une fonction qui donne la taille du pas suivant:
+% (b) Implementer une fonction qui donne la taille du pas suivant:
 % cf énoncé
+% Quelle est la nature de ce pas? D'ou cette formule vient-elle?
+
+% (c) Comparer le comportement de l'algorithme en utilisant les pas (a) et
+% (b)
+
+% (d) Comparer la methode de plus forte pente et la methode quasi-Newton
+% (qui est deja implementee - Serie 3).
 
 end
 
