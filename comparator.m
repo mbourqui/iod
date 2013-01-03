5e502ce926bc03a9ade411fe1cbb42bceacc6ecd
Constantin Victor <victor.constantin88@gmail.com>
2013-01-03 09:00:02 +0100

    Mise en forme des commentaires

--- comparator.m
+++ comparator.m
@@ -1,22 +1,24 @@
-%
-% Compare les deux méthodes pour un jeu de données commun.
-%
-% (adapter pfp_launcher et optSerie en fonction de ce script)
-%
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+% Compare les deux méthodes pour un jeu de données commun.		%
+%																%
+% (adapter pfp_launcher et optSerie en fonction de ce script)	%
+%																%
+% BOURQUI Marc													%	
+% CONSTANTIN Victor												%
+% SCHORI Ian                                                    %
+% SIMOND Floriant                                               %
+%                                                               %
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear;
 
-%
 % Valeurs initiales
-%
-
 fct = 'f';
 x(0)
 x0 = x(randsample(x(0),1)) % choisir ici une paire de valeurs prédéfinies dans x.m
 
 % Notre implémentation de pfp
-%pfp(fct, x0, 1); % Ne marche pas, car la fonction tp appelée dans pfpInnerLoop:16
-%manque.
+pfp(fct, x0, 1);
 
 % Solution de quasi Newton
 quasiNewton(fct, x0);
